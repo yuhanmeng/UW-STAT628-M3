407c13039834bfa8b9b4f6ec6c0fd04ae6c5e061
cwang647 <cwang647@wisc.edu>
2019-11-08 15:36:23 -0600

    Create workinghoursanalysis.R

---
+++ code/workinghoursanalysis.R
@@ -0,0 +1,23 @@
+data<-read.csv("workinghours.csv",header = TRUE,stringsAsFactors = FALSE) 
+head(data)
+data$stars=factor(data$stars)
+data$days=factor(data$days)
+
+par(mfrow=c(1,2),las=1,cex=1)
+par(cex.lab=1.6,cex.axis=1.6)
+boxplot(averageworkingtime~stars,data=data,xlab="stars",ylab="Average Workingtime per day")
+boxplot(totalworkingtime~stars,data=data,xlab="stars",ylab="Total Workingtime per week")
+
+ord1=polr(stars~averageworkingtime,data=data)
+ctable <- coef(summary(ord1))
+p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+ctable <- cbind(ctable, "p value" = p)
+ctable
+
+
+ord2=polr(stars~totalworkingtime,data=data)
+ctable <- coef(summary(ord2))
+p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+ctable <- cbind(ctable, "p value" = p)
+ctable
+
