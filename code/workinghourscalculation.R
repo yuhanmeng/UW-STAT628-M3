2d1a08f3017026ee37683368b8be7cb738be8f62
cwang647 <cwang647@wisc.edu>
2019-11-08 15:36:28 -0600

    Create workinghourscalculation.R

---
+++ code/workinghourscalculation.R
@@ -0,0 +1,45 @@
+data<-read.csv("PA_business_cleaning.csv",header = TRUE,stringsAsFactors = FALSE) 
+data2<-data[,c("hours","stars")]
+head(data2)
+
+averworklist=c()
+totalworklist=c()
+dayslist=c()
+for (i in 1:nrow(data2)){
+  s1=data2$hours[i]
+  s2=gsub("[{}\' ]","",s1)
+  s3=strsplit(s2,",")[[1]]
+  d=c()
+  for (strtime in strsplit(s3,"y:")){
+    times=strsplit(strtime[2],"-")[[1]]
+    work=-as.numeric(difftime(strptime(times[1], "%H:%M"),strptime(times[2], "%H:%M"),units='hours'))
+    if (work<=0){work=work+24}
+    d=c(d,work)
+  }
+  if (length(d)==0) 
+  {
+    averwork=NA
+    totalwork=NA
+    days=NA
+  }
+  else
+  {
+    averwork=mean(d)
+    totalwork=sum(d)
+    days=length(d)
+  }
+  averworklist=c(averworklist,averwork)
+  totalworklist=c(totalworklist,totalwork)
+  dayslist=c(dayslist,days)
+  if(i%%1000==0){print(i)}
+}
+
+data3=data.frame(stars=data2$stars,
+                 averageworkingtime=averworklist,
+                 totalworkingtime=totalworklist,
+                 days=dayslist);
+write.csv(data3,"workinghours.csv",row.names=FALSE)
+
+
+
+
