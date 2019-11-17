e998c40b0608f5e7f663ba5cb2c426c83cc0d03e
cwang647 <cwang647@wisc.edu>
2019-11-17 15:00:52 -0600

    Update FinalBusinessInfo.R

--- code/FinalBusinessInfo.R
+++ code/FinalBusinessInfo.R
@@ -3,4 +3,4 @@ topic<-read.csv("bus_with_topicindex.csv",header = TRUE,stringsAsFactors = FALSE
 topic=topic[,c("business","topicindex")]
 colnames(topic)[1]="business_id"
 finaldata=merge(data, topic, by = "business_id", all.x = T)
-write.csv(data5,"Finalbusiness.csv",row.names=FALSE)
+write.csv(finaldata,"Finalbusiness.csv",row.names=FALSE)
